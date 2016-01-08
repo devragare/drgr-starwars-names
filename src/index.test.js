63ef9acc6fb43f0eba1633b64319bfb4c945d1e5
Rafael Garcia (Ariadna) <rafa@myariadna.com>
2016-01-08 13:38:15 +0100

    Commitizen doesn't work revise

---
+++ src/index.test.js
@@ -0,0 +1,24 @@
+var expect = require('chai').expect;
+var starWarsNames = require('./index');
+
+describe('starwars-names', function(){
+	describe('all', function(){
+		it('should be an array of strings', function(){
+			expect(starWarsNames.all).to.satisfy(isArrayOfStrings);
+			function isArrayOfStrings(array){
+				return array.every(function(item){
+					return typeof item === 'string';
+				});
+			}
+		});
+		it('should contain Luke Skywalker', function(){
+			expect(starWarsNames.all).to.include('Luke Skywalker');
+		});
+	});
+	describe('random', function(){
+		it('should return a random item from starWarsNames.all', function(){
+			var item = starWarsNames.random();
+			expect(starWarsNames.all).to.include(item);
+		});
+	});
+});
