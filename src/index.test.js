3c5486b9cd872e81e135c1f4a9dfe18b2cfeb456
ragare <ragare@gmail.com>
2016-01-10 11:31:02 +0100

    feat(random): Get an array of random names
    Now you can choose the array legth
    
    closes #2

--- src/index.test.js
+++ src/index.test.js
@@ -1,24 +1,32 @@
 var expect = require('chai').expect;
 var starWarsNames = require('./index');
 
-describe('starwars-names', function(){
-	describe('all', function(){
-		it('should be an array of strings', function(){
-			expect(starWarsNames.all).to.satisfy(isArrayOfStrings);
-			function isArrayOfStrings(array){
-				return array.every(function(item){
-					return typeof item === 'string';
-				});
-			}
-		});
-		it('should contain Luke Skywalker', function(){
-			expect(starWarsNames.all).to.include('Luke Skywalker');
-		});
-	});
-	describe('random', function(){
-		it('should return a random item from starWarsNames.all', function(){
-			var item = starWarsNames.random();
-			expect(starWarsNames.all).to.include(item);
-		});
-	});
+describe('starwars-names', function() {
+    describe('all', function() {
+        it('should be an array of strings', function() {
+            expect(starWarsNames.all).to.satisfy(isArrayOfStrings);
+
+            function isArrayOfStrings(array) {
+                return array.every(function(item) {
+                    return typeof item === 'string';
+                });
+            }
+        });
+        it('should contain Luke Skywalker', function() {
+            expect(starWarsNames.all).to.include('Luke Skywalker');
+        });
+    });
+    describe('random', function() {
+        it('should return a random item from starWarsNames.all', function() {
+            var item = starWarsNames.random();
+            expect(starWarsNames.all).to.include(item);
+        });
+        it('should return an array of random names if passed a number', function() {
+            var randomItems = starWarsNames.random(3);
+            expect(randomItems).to.have.length(3);
+            randomItems.forEach(function(item) {
+                expect(starWarsNames.all).to.include(item);
+            });
+        });
+    });
 });
